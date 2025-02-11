7bd7a62b92d198f5c81f5376083698a282afeb92
Dipto Roy <diptoray292@gmail.com>
2025-02-11 07:34:47 +0600

    verify token,verfify admin

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let cors = require('cors');
 let app = express();
+let jwt = require('jsonwebtoken');
 app.use(cors());
 app.use(express.json());
 let port = process.env.PORT || 5000;
@@ -19,16 +20,61 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
+        let verifyToken = (req, res, next) => {
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'forbidden access' })
+            }
+            let token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+                if (error) {
+                    return res.status(401).send({ message: 'forbidden access' })
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
+
+        let verifyAdmin = async (req, res, next) => {
+            let email = req.decoded.email;
+            let query = { email: email };
+            let user = await userDb.findOne(query);
+            let isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+            next();
+        }
+
         let menuItemsDB = client.db('Bistro-Boss-DB').collection('menu');
         let cartItemsDB = client.db('Bistro-Boss-DB').collection('carts');
         let userDb = client.db('Bistro-Boss-DB').collection('users');
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        app.get('/user', async (req, res) => {
+        app.post('/jwt', async (req, res) => {
+            let user = req.body;
+            let token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token });
+        })
+        app.get('/user/admin/:email', verifyToken, async (req, res) => {
+            let email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'unauthorized access' })
+            }
+
+            let query = { email: email };
+            let user = await userDb.findOne(query);
+            let admin = false;
+            if (user) {
+                admin = user?.role === 'admin'
+            }
+            res.send({ admin });
+        })
+        app.get('/user', verifyToken, async (req, res) => {
+            console.log(req.headers)
             let result = await userDb.find().toArray();
             res.send(result);
         })
-        app.delete('/user/:id', async (req, res) => {
+        app.delete('/user/:id', verifyToken, verifyAdmin, async (req, res) => {
             let id = req.params.id;
             let query = { _id: new ObjectId(id) };
             let result = await userDb.deleteOne(query);
@@ -36,7 +82,7 @@ async function run() {
 
         })
 
-        app.patch('/user/admin/:id', async (req, res) => {
+        app.patch('/user/admin/:id', verifyAdmin, verifyToken, async (req, res) => {
             let id = req.params.id;
             let filter = { _id: new ObjectId(id) };
             let updateDoc = {
